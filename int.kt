6081605cbd36b3070fee9887d1b7fd3b0f3285e3
cristianbrunone <cristianbrunonecmbc@gmail.com>
2025-01-05 15:54:04 -0300

    Integer en kotlin

---
+++ int.kt
@@ -0,0 +1,17 @@
+//Ejemplos de conversion a Double
+
+val numero: Int = 42
+val numeroDoble: Double =  numero.toDouble()
+
+println(numeroDoble)
+
+//Ejemplos de conversion a String
+
+val numero: Int = 42
+val numeroTexto: String = numero.toString()
+println(numeroTexto)
+
+//Ejemplos de conversion a Long
+val numero: Int = 42
+val numeroLargo: Long = numero.toLong()
+println(numeroLargo)
